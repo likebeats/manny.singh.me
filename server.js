7c37d01d344c6810a0018d1630d84d88f02b752d
Manny Singh <manny.singh@yikes.co>
2017-04-11 16:32:07 -0400

    add project endpoints

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var PROJECTS_COLLECTION = "projects";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// PROJECTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+app.get("/api/projects", function(req, res) {
+  db.collection(PROJECTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get projects.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.get("/api/projects/:id", function(req, res) {
+  db.collection(PROJECTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get project");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
