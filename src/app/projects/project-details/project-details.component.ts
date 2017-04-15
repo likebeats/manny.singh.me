28ea22af1d5ba54b2d1490750734ec3cdb648d25
Manny Singh <manny.singh@yikes.co>
2017-04-14 20:06:15 -0400

    add project details

--- src/app/projects/project-details/project-details.component.ts
+++ src/app/projects/project-details/project-details.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Project } from '../project';
+import { ProjectService } from '../project.service';
+import { Router, ActivatedRoute } from '@angular/router';
 import { slideInDownAnimation } from '../../animations';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.css'],
+  providers: [ProjectService],
   animations: [ slideInDownAnimation ]
 })
-export class ProjectDetailsComponent {
+export class ProjectDetailsComponent implements OnInit {
 
   @HostBinding('@routeAnimation') routeAnimation = true;
   @HostBinding('style.display')   display = 'block';
@@ -17,6 +20,16 @@ export class ProjectDetailsComponent {
   @Input()
   project: Project;
 
-  constructor() {}
+  constructor(private projectService: ProjectService,
+              private route: ActivatedRoute,
+              private router: Router) {}
+
+  ngOnInit() {
+    const projectId = this.route.snapshot.params['id'];
+    this.projectService.getProjectById(projectId)
+                       .then((project: Project) => {
+                         this.project = project;
+                       });
+  }
 
 }
